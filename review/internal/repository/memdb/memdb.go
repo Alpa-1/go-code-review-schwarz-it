990fe4c1cc0022f8261edb046a730ca374c275c2
EPG\MIC <michael.siegle@epg.com>
2024-05-30 23:08:33 +0200

    refactor: introduce a rw mutex to memdb

--- review/internal/repository/memdb/memdb.go
+++ review/internal/repository/memdb/memdb.go
@@ -3,6 +3,7 @@ package memdb
 import (
 	"coupon_service/internal/service/entity"
 	"fmt"
+	"sync"
 )
 
 type repository interface {
@@ -11,6 +12,7 @@ type repository interface {
 }
 
 type Repository struct {
+	mu      sync.RWMutex
 	entries map[string]entity.Coupon
 }
 
@@ -19,14 +21,19 @@ func New() *Repository {
 }
 
 func (r *Repository) FindByCode(code string) (*entity.Coupon, error) {
+	r.mu.RLock()
+	defer r.mu.RUnlock()
 	coupon, ok := r.entries[code]
 	if !ok {
-		return nil, fmt.Errorf("Coupon not found")
+		return nil, fmt.Errorf("coupon not found")
 	}
 	return &coupon, nil
 }
 
 func (r *Repository) Save(coupon entity.Coupon) error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
 	r.entries[coupon.Code] = coupon
 	return nil
 }
