212ba47d06e3b36d16f099ec9c3c179d8e8bc729
EPG\MIC <michael.siegle@epg.com>
2024-05-31 00:36:02 +0200

    feat: add status codes for failed requests

--- review/internal/api/coupon.go
+++ review/internal/api/coupon.go
@@ -10,10 +10,13 @@ import (
 func (a *API) Apply(c *gin.Context) {
 	apiReq := ApplicationRequest{}
 	if err := c.ShouldBindJSON(&apiReq); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
+
 	}
 	basket, err := a.svc.ApplyCoupon(apiReq.Basket, apiReq.Code)
 	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
@@ -23,22 +26,26 @@ func (a *API) Apply(c *gin.Context) {
 func (a *API) Create(c *gin.Context) {
 	apiReq := Coupon{}
 	if err := c.ShouldBindJSON(&apiReq); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 	err := a.svc.CreateCoupon(apiReq.Discount, apiReq.Code, apiReq.MinBasketValue)
 	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	c.Status(http.StatusOK)
+	c.JSON(http.StatusCreated, gin.H{"code": apiReq.Code})
 }
 
 func (a *API) Validate(c *gin.Context) {
 	apiReq := CouponRequest{}
 	if err := c.ShouldBindJSON(&apiReq); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 	coupon, err := a.svc.ValidateCoupon(apiReq.Code)
 	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 	c.JSON(http.StatusOK, coupon)
